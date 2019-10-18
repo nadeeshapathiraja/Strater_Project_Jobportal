105016b3ddb20c05f612bd2ff7f941c4985bd3cf
nadeeshapathiraja <pdncpathiraja95@gmail.com>
2019-10-18 15:36:05 +0530

    add crud for candidate language

--- app/Http/Controllers/CandidateLanguageController.php
+++ app/Http/Controllers/CandidateLanguageController.php
@@ -46,20 +46,36 @@ class CandidateLanguageController extends Controller
     }
 
     
-    public function edit(candidate_language $candidate_language)
+    public function edit($candidate_lang_id)
     {
-        //
+        $candidate_language = candidate_language::find($candidate_lang_id);
+        return view('candidate_languages.edit', compact('candidate_language'));
     }
 
     
-    public function update(Request $request, candidate_language $candidate_language)
+    public function update(Request $request,$candidate_lang_id)
     {
-        //
+        $request->validate([
+            
+        ]);
+
+        $candidate_language = candidate_language::find($candidate_lang_id);
+        $candidate_language->language_code =  $request->get('language_code');
+        $candidate_language->spoken_level = $request->get('spoken_level');
+        $candidate_language->written_level = $request->get('written_level');
+        $candidate_language->default = $request->get('default');
+
+        $candidate_language->save();
+
+        return redirect('/candidate_languages')->with('success', 'Update Successfull!');
     }
 
     
-    public function destroy(candidate_language $candidate_language)
+    public function destroy($candidate_lang_id)
     {
-        //
+        $candidate_language = candidate_language::find($candidate_lang_id);
+        $candidate_language->delete();
+
+        return redirect('/candidate_languages')->with('success', 'Account Deleted!');
     }
 }
