cf27e471370bd3694ab508f5e1d86cffb1d121b7
nadeeshapathiraja <pdncpathiraja95@gmail.com>
2019-10-18 10:07:11 +0530

    Start candidate application

---
+++ app/Http/Controllers/CandidateApplicationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\candidate_application;
+use Illuminate\Http\Request;
+
+class CandidateApplicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\candidate_application  $candidate_application
+     * @return \Illuminate\Http\Response
+     */
+    public function show(candidate_application $candidate_application)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\candidate_application  $candidate_application
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(candidate_application $candidate_application)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\candidate_application  $candidate_application
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, candidate_application $candidate_application)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\candidate_application  $candidate_application
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(candidate_application $candidate_application)
+    {
+        //
+    }
+}
